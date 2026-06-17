1afe201a82188a1d59234d364bf6fae7d37ad047
kim <oliveirakemberly3@gmail.com>
2026-06-17 14:49:39 -0300

    NpN

---
+++ src/main/java/com/example/projetoN/execicioN/ExecicioNApplication.java
@@ -0,0 +1,15 @@
+package com.example.projetoN.execicioN;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExecicioNApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExecicioNApplication.class, args);
+	}
+
+}
+
+//  mvn spring-boot:run
---
+++ src/test/java/com/example/projetoN/execicioN/ExecicioNApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projetoN.execicioN;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExecicioNApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
